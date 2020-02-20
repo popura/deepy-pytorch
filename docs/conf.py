310a6ab194b889db08c3c32a29f7a2d9489b140e
popura <popura.net@gmail.com>
2020-02-20 16:13:51 +0900

    debug

--- docs/conf.py
+++ docs/conf.py
@@ -20,8 +20,8 @@ sys.path.insert(0, os.path.abspath('../deepy/'))
 project = 'deepy'
 copyright = '2020, popura'
 author = 'popura'
-version = '0.0.3'
-release = '0.0.3'
+version = '0.0.4'
+release = '0.0.4'
 
 
 # -- General configuration ---------------------------------------------------
