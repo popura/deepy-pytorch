ee7af8bc0241a3324eb04e209898bcd5c380540f
popura <popura.net@gmail.com>
2020-09-26 09:12:15 +0900

    bug fixed

--- deepy/nn/model/resnet.py
+++ deepy/nn/model/resnet.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchsummary import summary
-from deepy import layer
+from deepy.nn import layer
 
 
 class ResNet(nn.Module):
